6da4882476662150f91b92c2df2398cc70b043ca
hugo395 <hugofsrodrigues@gmail.com>
2020-03-26 19:01:21 +0000

    done

---
+++ JS.js/other.js
@@ -0,0 +1,123 @@
+// let questions = [{
+//     question: "Which insurance covers fire and burglary?",
+//     answers: [
+//         { isCorrect: true, text: "Home insurance" },
+//         { isCorrect: false, text: "Liability insurance" },
+//         { isCorrect: false, text: "Travel insurance" },
+//         { isCorrect: false, text: "Life insurance" },
+//     ]
+//   },{
+//     question: "What is gross salary",
+//     answers: [
+//         { isCorrect: false, text: "The amount of salary that you actually receive" },
+//         { isCorrect: true, text: "The amount that is agreed upon in the employment contract" },
+//         { isCorrect: false, text: "The difference between personl income and taxable income" },
+//         { isCorrect: false, text: "The difference between net income and taxable income" }
+//     ]
+//   },{
+//     question: "What does inflation mean?",
+//     answers: [
+//         { isCorrect: false, text: "Prices decrease" },
+//         { isCorrect: false, text: "More value of money due to sustained decrease in prices" },
+//         { isCorrect: true, text: "Less value of money due to sustained increase in prices" },
+//         { isCorrect: false, text: "That you have to pay in cash" },
+//     ]
+//   },{
+//     question: "What of this isn't a type of Loans offered by banks?",
+//     answers: [
+//         { isCorrect: false, text: "Mortgage Loans" },
+//         { isCorrect: false, text: "Secured Personal Loan" },
+//         { isCorrect: true, text: "Credit Card" },
+//         { isCorrect: false, text: "Unsecured Personal Loan" },
+//     ]
+//   },{
+//     question: "What is an expense?",
+//     answers: [
+//         { isCorrect: false, text: "A bank transfer" },
+//         { isCorrect: true, text: "An obligation to pay for services/goods" },
+//         { isCorrect: false, text: "An obligation to receive money" },
+//         { isCorrect: false, text: "A money deposit" },
+//     ]
+//   },{
+//     question: "Why should you think twice about getting a personal loan?",
+//     answers: [
+//         { isCorrect: false, text: "Because these loans do not exist" },
+//         { isCorrect: true, text: "Because these loans are often very expensive" },
+//         { isCorrect: false, text: "Because you always get scammed" },
+//         { isCorrect: false, text: "Because these loans mean you have to sell your car" },
+//     ]
+//   },{
+//     question: "What is VAT?",
+//     answers: [
+//         { isCorrect: true, text: "A tax that is on nearly all goods" },
+//         { isCorrect: false, text: "A grant you receive from the government" },
+//         { isCorrect: false, text: "A tax only for goods over €100" },
+//         { isCorrect: false, text: "A tax only for soda and candy" },
+//     ]
+//   },{
+    
+//     question: "What should you spread in order to reduce risk?",
+//     answers: [
+//         { isCorrect: false, text: "Budgets" },
+//         { isCorrect: true, text: "Investments" },
+//         { isCorrect: false, text: "Cash money" },
+//         { isCorrect: true, text: "Deposits" },
+//     ]
+//   },{
+//     question: "Which expenditure should receive the highest priority",
+//     answers: [
+//         { isCorrect: false, text: "Ironbeers" },
+//         { isCorrect: false, text: "Netflix subscription" },
+//         { isCorrect: true, text: "Rent payment" },
+//         { isCorrect: false, text: "Ticket's for a Benfica Game" },
+//     ]
+//   },{
+//     question: "You are saving €120 with an interest rate of 2% per year. After 3 years, how much do you have?",
+//     answers: [
+//         { isCorrect: false, text: "Less that €127" },
+//         { isCorrect: true, text: "More €127" },
+//         { isCorrect: false, text: "Exactly €127" },
+//         { isCorrect: false, text: "How? I don't have any money to save" },
+//     ]
+//   },{
+//     question: "If you receive a suspects message from your supposed bank, what should you do?",
+//     answers: [
+//         { isCorrect: false, text: "It is safe, click the link and enter your details" },
+//         { isCorrect: false, text: "If the link contains your bank name, it is safe" },
+//         { isCorrect: false, text: "Ignore the message, do nothing" },
+//         { isCorrect: true, text: "Do not click, call your bank" },
+//     ]
+//   },{
+//     question: "Which debt should you try to pay back first?",
+//     answers: [
+//         { isCorrect: true, text: "The debt with the highest interest rate" },
+//         { isCorrect: false, text: "The highest debt" },
+//         { isCorrect: false, text: "The lowest debt" },
+//         { isCorrect: false, text: "The debt with the lowest interest rate" },
+//     ]
+//   },{
+//     question: "Which of the following is NOT a debt",
+//     answers: [
+//         { isCorrect: true, text: "A stock" },
+//         { isCorrect: false, text: "Mortage" },
+//         { isCorrect: false, text: "Telephone subscription with a telephone" },
+//         { isCorrect: false, text: "When your parents lend you €100" },
+//     ]
+//   },{
+//     question: "Someone on Instagram asks you in a message to open a bank account for them, what do you do?",
+//     answers: [
+//         { isCorrect: true, text: "Block the account and report it to Instagram" },
+//         { isCorrect: false, text: "Open the bank account, DM your PIN, give him your bank card" },
+//         { isCorrect: true, text: "Report it to the police" },
+//         { isCorrect: false, text: "Nothing" },
+//     ]
+//   },{
+//     question: "In 2008, 300 billion Zimbabwe dollars equaled aproximately €1. What happened?",
+//     answers: [
+//         { isCorrect: false, text: "The euro was doing very well" },
+//         { isCorrect: false, text: "It has always been like this" },
+//         { isCorrect: false, text: "HyperDEflation" },
+//         { isCorrect: true, text: "HyperINflation" },
+//     ]
+//   }
+//   ]
---
+++ JS.js/questions.js
@@ -0,0 +1,5 @@
+// class questions{
+//     constructor(questions){
+//         this.questions = questions
+//     }
+// }
